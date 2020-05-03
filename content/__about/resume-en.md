916ef3584e4ab90ede877d8865bf4c38a3b498ac
hong-dev <jjuggumih@gmail.com>
2020-05-04 02:32:53 +0900

    change profile content

--- content/__about/resume-en.md
+++ content/__about/resume-en.md
@@ -4,10 +4,41 @@ date: 2019-1-27 16:21:13
 lang: 'en'
 ---
 
-# Name
+
 
 <div align="center">
 
-_Thank you for reading my resume. If you want to contact me, Please send me an email._
+# HONG_DEV
+
+<img src="https://user-images.githubusercontent.com/53142539/80919488-19c9d780-8da5-11ea-8b12-3d98543d4112.JPG" alt="HONG" width=220px style="border-radius:50%">
+<br>
+<p style="font-weight:bold; font-size:1.3em">#python</p>
+<br>
 
 </div>
+
+</div>
+<div style="margin-left:12%">
+
+<span style="font-weight:bold">Github:</span> [hong-dev](https://github.com/hong-dev)\
+<span style="font-weight:bold">Instagram:</span> [hong._.dev](https://www.instagram.com/hong._.dev/)\
+<span style="font-weight:bold">Email:</span> jjuggumih@gmail.com
+
+</div>
+
+<br>
+<br>
+
+<div style="margin-left:12%; margin-right:12%">
+
+:fire: 좌우명은 해보고 후회하자! 무의미한 경험은 없다고 생각합니다.
+
+:earth_americas: 취미는 취미찾기! 새로운 것을 보고 듣고 배우는 것을 좋아합니다.
+
+:musical_note: 다같이 으샤으샤하며 재밌게 일하는 게 좋습니다.
+
+:rocket: 자비스와 함께 일하는 게 꿈입니다 :)
+
+
+
+
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `hong_devlog`,
+  description: `Devlog`,
+  author: `hong._.dev`,
+  introduction: `Trying to be a good developer`,
+  siteUrl: `https://hong-dev.github.io/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `hong-dev`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
@@ -15,16 +15,16 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'hong-dev/hong-dev.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'G-KX83R3N1E8', // Add your google analytics tranking ID
 }
