2ad10ef31a28140f82c106d6fd110f66d1afe8f1
hong-dev <jjuggumih@gmail.com>
2020-05-04 17:58:10 +0900

    change google analytics id

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -26,5 +26,5 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: 'G-KX83R3N1E8', // Add your google analytics tranking ID
+  ga: 'UA-165390603-1', // Add your google analytics tranking ID
 }
